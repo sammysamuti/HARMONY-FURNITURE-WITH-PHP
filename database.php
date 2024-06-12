c00a4d94d7578417eca6f02de1ded78a92acc46a
Nahom ketsela <130212420+Nahom-ketsela@users.noreply.github.com>
2024-06-12 12:02:45 +0300

    Add files via upload

---
+++ database.php
@@ -0,0 +1,19 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "123456789@abc";
+$dbname = "Harmony_furniture";
+try {
+    $conn = new PDO("mysql:host=$servername", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+    $sql = "CREATE DATABASE IF NOT EXISTS $dbname ";
+    $conn->exec($sql);
+    echo "Database created successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+$conn = null;
---
+++ databasetTables.php
@@ -0,0 +1,56 @@
+<?php
+include '../PHP/database.php';
+
+try {
+    // database connection
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+    $sql = "CREATE TABLE Users (
+        user_id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(50) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        email VARCHAR(100) NOT NULL,
+        isadmin TINYINT(1) DEFAULT 0
+    )";
+    $conn->exec($sql);
+
+
+    $sql = "CREATE TABLE Products (
+        product_id INT AUTO_INCREMENT PRIMARY KEY,
+        product_name VARCHAR(100) NOT NULL,
+        description TEXT,
+        price DECIMAL(10, 2) NOT NULL,
+        stock_quantity INT NOT NULL,
+        image_url VARCHAR(255)
+    )";
+    $conn->exec($sql);
+
+    $sql = "CREATE TABLE Orders (
+        order_id INT AUTO_INCREMENT PRIMARY KEY,
+        user_id INT,
+        order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+        total_amount DECIMAL(10, 2) NOT NULL,
+        payment_status VARCHAR(20) NOT NULL,
+        delivery_status VARCHAR(20) NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    )";
+    $conn->exec($sql);
+
+    // SQL query to create Favorites table
+    $sql = "CREATE TABLE Favorites (
+        favorite_id INT AUTO_INCREMENT PRIMARY KEY,
+        user_id INT,
+        product_id INT,
+        FOREIGN KEY (user_id) REFERENCES Users(user_id),
+        FOREIGN KEY (product_id) REFERENCES Products(product_id)
+    )";
+    $conn->exec($sql);
+
+    echo "Tables created successfully";
+} catch (PDOException $e) {
+    echo "Error creating tables: " . $e->getMessage();
+}
+
+$conn = null;
